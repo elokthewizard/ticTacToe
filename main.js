8d678e433d6cb31e7f76408662ac46da1f9f3bc3
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2024-01-16 20:05:50 -0700

    add message stating who went and who will go next

--- main.js
+++ main.js
@@ -36,15 +36,24 @@ ticTacToe = (() => {
 
             playRound = (x, y) => {
 
-                board[x][y].value == 0 && board[x][y] !== playerList[1].gameSymbol ? board[x][y].value = currentPlayer.gameSymbol : false;
+                (board[x][y].value !== playerList[1].gameSymbol) && (board[x][y].value == 0) ? board[x][y].value = currentPlayer.gameSymbol : false;
+
+                //check win condition
+                (() => { 
+                    for (let k = 0; k < board.length; k++) {
+                        if (board[k][0].value === board[k][1].value === board[k][2].value) {
+                            console.log(`${currentPlayer.name}` + 'wins!')
+                        }
+                    }
+                })();
 
                 updateDisplay();
 
                 //swapPlayer 
                 (() => {
-                    console.log(currentPlayer)
+                    console.log(`${currentPlayer.name} played ${currentPlayer.gameSymbol}`)
                     currentPlayer = currentPlayer.name === 'player1' ? currentPlayer = playerList[1]: currentPlayer = playerList[0];
-                    console.log(currentPlayer)
+                    console.log(`${currentPlayer.name}, it is now your turn`)
                 })();
 
             };
